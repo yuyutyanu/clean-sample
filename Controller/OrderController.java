88539570a85ab5605dd8a0be646e652bde5666fe
chanu <yuyutyanu@yahoo.co.jp>
2019-04-07 13:28:09 +0900

    update

--- Controller/OrderController.java
+++ Controller/OrderController.java
@@ -3,6 +3,7 @@ import UseCase.OrderInputData;
 import UseCase.Order;
 
 public class OrderController {
+	//DI: usecase interface
 	public void handle() {
 		new Order().handle(new OrderInputData(1000));
 	}
